35e6df84b8212cb1717c81343bac599ab71fe726
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2025-10-15 18:24:48 +0300

    Added .env

--- src/constants/contants.ts
+++ src/constants/contants.ts
@@ -1,5 +1,4 @@
 const SHEET_ID = "1nUBVNWKdiJGc5OKANwiniPUrHQUdIfi7pG5GKlNqpEc";
-const SHEET_NAME = "Аркуш1";
 const DOU_URL = "https://dou.ua/calendar/tags/менеджмент/";
 
-export { SHEET_ID, SHEET_NAME, DOU_URL };
+export { SHEET_ID, DOU_URL };
--- src/utils/get-sheets-client.util.ts
+++ src/utils/get-sheets-client.util.ts
@@ -1,14 +1,21 @@
 import { google, sheets_v4 } from "googleapis";
-import fs from "fs/promises";
+import dotenv from "dotenv";
+dotenv.config();
+
+const CLIENT_EMAIL = process.env.CLIENT_EMAIL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+if (!CLIENT_EMAIL || !PRIVATE_KEY) {
+  throw new Error(
+    "Missing CLIENT_EMAIL or PRIVATE_KEY in environment variables"
+  );
+}
 
 // === GOOGLE AUTH ===
 async function getSheetsClient(): Promise<sheets_v4.Sheets> {
-  const credentialsRaw = await fs.readFile("./credentials.json", "utf8");
-  const credentials = JSON.parse(credentialsRaw);
-
   const auth = new google.auth.JWT({
-    email: credentials.client_email,
-    key: credentials.private_key,
+    email: CLIENT_EMAIL,
+    key: PRIVATE_KEY,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
